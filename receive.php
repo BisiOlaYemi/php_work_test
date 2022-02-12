0ce5ff940701894eae8a35d8a051d0437c45ea84
OLAYEMI OLUBISI <46648435+BisiOlaYemi@users.noreply.github.com>
2022-02-12 14:56:36 -0800

    Update php_work

--- receive.php
+++ receive.php
@@ -1,6 +1,7 @@
 <?php
 
-if(isset($_POST ['Submit'])) {
+ 
+if(isset($_POST ['Submit'])){
     $pickup=$_POST['pickup']; 
     $dropoff=$_POST['dropoff']; 
     $fullname=$_POST['fullname']; 
@@ -9,25 +10,33 @@ if(isset($_POST ['Submit'])) {
     $cars=$_POST['cars']; 
     $vehicletype=$_POST['vehicletype'];
     $pickupdate=$_POST['pickupdate']; 
-    $dropoffdate=$_POST['dropoffdate']; 
-    $timeset=$_POST['timeset']; 
+     $dropoffdate=$_POST['dropoffdate']; 
+     $timeset=$_POST['timeset']; 
 
     $to='frankolayemi@gmail.com';
     $subject='Booking Submission';
-    $message="pickup: ".$pickup. "\n". "dropoff: " .$dropoff. "\n". "wrote the following: " ."\n\n" .$timeset;
-    $headers= "From: ".$email;
+    $message="pickup: ".$pickup. "\n"
+	. "dropoff: ".$dropoff. "\n"
+	. "fullname: ".$fullname. "\n"
+	. "phone: ".$phone. "\n"
+	. "cars: ".$cars. "\n"
+	. "vehicletype: ".$vehicletype. "\n"
+	. "dropoffdate: ".$dropoffdate. "\n"
+	. "dropoff: ".$dropoff. "\n"
+	."wrote the following: " ."\n\n" .$timeset;
+    $headers= "From: ".$email_from;
 
-}
-    if(mail($to, $subject, $message, $headers)){
-        echo "Booking Acknowledged Thank You!.$pickup";
+
+
+	if(!mail($to, $subject, $message, $headers)) {
+		print "error";
+    }else {
+		print  "Booking Acknowledged Thank You! for".$fullname;
     }
-        else {
+    
 
-            echo "Something Went Wrong";
 
-        
-    }
-
+} // if submit closeing
 
 
 
